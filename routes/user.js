3540b747cec3ca52f97832f100cf2ba48cf9f643
Anuj Shashimal <anujshashimal456@gmail.com>
2020-09-27 23:23:10 +0530

    user route updated

--- routes/user.js
+++ routes/user.js
@@ -6,20 +6,20 @@ const user = require('../services/user');
 router.post('/registerUsers', async( req, res) => {
     try{
         const body = req.body;
-        const userDet = await user.RegisterUsers(body);
-        res.send(userDet);
-        res.status(200).send("User Registered!");
+        await user.RegisterUsers(body);
+        res.status(200).send("success!")
     }catch (e){
         res.status(404).send({description:e.message})
     }
 })
+
 //login as sitemanager and supplier
 router.post('/userLogin', async( req, res) => {
     try{
         const body = req.body;
         const users = await user.LoginUser(body);
         console.log("USERRR", users);
-        res.send(users);
+        res.json(users);
     }catch (e){
         res.status(404).send({description:e.message})
     }
